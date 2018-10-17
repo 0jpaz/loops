2fa8d35e0759380656945813da51350fa5922410
0jpaz <43248467+0jpaz@users.noreply.github.com>
2018-10-17 14:01:34 -0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,33 @@
+// For-loop functions
+
+function positiveIntSum(limit){
+    let sum = 0;
+    let n = 100;
+    for (let i= 1; i<= limit ; i ++){
+    sum = sum + i ;
+}
+return sum;
+
+
+function additionPatternSum(start diff,limit){
+let start ; let diff ; 
+for (let i = start; let i<= limit ; i = i + diff){
+    sum = sum + i;
+return diff
+}
+
+
+
+
+// While-loop function
+
+function multiplicationPatternSum(){
+
+
+}
+
+
+
+
+
+//Later copy paste the checking code below here.
